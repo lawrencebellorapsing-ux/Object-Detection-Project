a45d58e78659578e626fea7260ed5a9f7e51209e
lawrencebellorapsing-ux <lawrencebellorapsing@gmail.com>
2026-05-21 16:07:03 -1200

    Update app.py

--- app.py
+++ app.py
@@ -2,31 +2,20 @@ import streamlit as st
 import cv2
 import numpy as np
 from ultralytics import YOLO
+from PIL import Image
 
 st.title("Real-Time Object Detection and Tracking")
 
 model = YOLO("yolov8n.pt")
 
-camera = cv2.VideoCapture(0)
+camera = st.camera_input("Open Camera")
 
-run = st.checkbox("Start Camera")
-
-frame_window = st.image([])
-
-while run:
-    success, frame = camera.read()
-
-    if not success:
-        st.write("Failed to access camera")
-        break
+if camera is not None:
+    image = Image.open(camera)
+    frame = np.array(image)
 
     results = model(frame)
 
     annotated_frame = results[0].plot()
 
-    frame_window.image(
-        annotated_frame,
-        channels="BGR"
-    )
-
-camera.release()
+    st.image(annotated_frame, channels="BGR")
